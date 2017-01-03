863aafa46a9f7bd3e079c0b1a57901dc976f8ccc
tobi <eiss.tobias@googlemail.com>
2017-01-03 09:22:32 +0100

    fix: remove own dependecies

--- RequestChainHandler.go
+++ RequestChainHandler.go
@@ -3,14 +3,13 @@ package goMiddlewareChain
 import (
 	"net/http"
 
-	"digital.fino/contractsafe-api/middleware"
 	"github.com/julienschmidt/httprouter"
 )
 
 // RequestChainHandler chains all handler
-func RequestChainHandler(responseHandler middleware.ResponseHandler, handlers ...middleware.Handler) httprouter.Handle {
+func RequestChainHandler(responseHandler ResponseHandler, handlers ...Handler) httprouter.Handle {
 	return httprouter.Handle(func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
-		payload := middleware.Response{}
+		payload := Response{}
 
 		// iterate all handlers
 		for _, handler := range handlers {
